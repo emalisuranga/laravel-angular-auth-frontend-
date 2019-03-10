1a458c5388fbf55c523532529fa3001e5f660deb
Emal <emalisuranga94@gmail.com>
2019-03-10 21:05:24 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { LoginComponent } from './component/login/login.component';
+import { SingupComponent } from './component/singup/singup.component';
+import { ProfileComponent } from './component/profile/profile.component';
+import { ResponseResetComponent } from './component/password/response-reset/response-reset.component';
+import { RequestResetComponent } from './component/password/request-reset/request-reset.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    SingupComponent,
+    ProfileComponent,
+    ResponseResetComponent,
+    RequestResetComponent
   ],
   imports: [
     BrowserModule,
